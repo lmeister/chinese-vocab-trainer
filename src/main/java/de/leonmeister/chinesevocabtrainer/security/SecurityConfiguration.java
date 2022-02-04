c0caae8da6a023aa88395f30addd0f2c60687349
Leon Meister <leonmeister97@gmail.com>
2022-02-04 10:51:59 +0100

    Add logout button, implement authentication for certain apis, use environmental variables for retrieving user info

--- src/main/java/de/leonmeister/chinesevocabtrainer/security/SecurityConfiguration.java
+++ src/main/java/de/leonmeister/chinesevocabtrainer/security/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package de.leonmeister.chinesevocabtrainer.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,16 +13,36 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private Environment environment;
+
+    // This specifies some users
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("bla")
-                .password("bla")
-                .roles("USER");
+                .withUser(environment.getProperty("student"))
+                .password(environment.getProperty("studentPassword"))
+                .roles("STUDENT")
+                .and()
+                .withUser(environment.getProperty("admin"))
+                .password(environment.getProperty("adminPassword"))
+                .roles("ADMIN");
     }
 
+    // This configures which encoding to use
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance(); // Using clear text password
+    }
+
+    // This configures which api requires which role
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/add").hasAnyRole("STUDENT", "ADMIN")
+                .antMatchers("/delete/**").hasRole("ADMIN")
+                .antMatchers("/").permitAll()
+                .antMatchers("/random").permitAll()
+                .and().formLogin();
     }
 }
